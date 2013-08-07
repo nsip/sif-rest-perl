18fa464e6603f089a0c71c93b52f45f080a205fe
Scott Penrose <scottp@dd.com.au>
2013-08-07 11:19:05 +1000

    Working across each environment

--- lib/SIF/REST.pm
+++ lib/SIF/REST.pm
@@ -1,14 +1,11 @@
 package SIF::REST;
 use warnings;
 use strict;
-use HTTP::Request::Common qw(GET POST);  
-use LWP::UserAgent; 
 use XML::Simple;
 use Mouse;
 use Mouse::Util::TypeConstraints;
-use Regexp::Common qw /URI/;
-
-use base qw/REST::Client/;
+use REST::Client;
+use MIME::Base64;
 
 =head1 NAME
 
@@ -20,11 +17,6 @@ SIF::REST - A low level SIF REST Client
 
 =cut
 
-# XXX NOTES
-# 	- Base class = REST, get, put, post etc
-# 	- Next = Environmen Create
-# 	- Next = Australian specific
-
 # URL Matching
 subtype 'URL'
     => as 'Str'
@@ -33,91 +25,141 @@ subtype 'URL'
 
 # Required - endpoint
 has endpoint => (
-	is => 'ro',
+	is => 'rw',
 	required => 1,
 	isa => 'URL',
 );
 
 # Optional - Uset Agent
-has ua => {
-	is => 'rw',
-	required => 1,
-	default => sub {
-		LWP::UserAgent->new();
-	},
-};
-
-has sessionToken => {
+has rest => (
 	is => 'rw',
 	required => 1,
 	lazy => 1,
 	default => sub {
 		my ($self) = @_;
-		return $self->environment_create();
+		my $client = REST::Client->new();
+		$client->setHost($self->endpoint);
+		$client->addHeader(Content_Type => 'application/xml');
+		$client->addHeader(Accept => 'application/xml');
+		return $client;
 	},
-};
+);
 
-has sharedSecret => {
+# sessionToken - created as required, or can also be provided
+has sessionToken => (
+	is => 'rw',
+	required => 1,
+	lazy => 1,
+	default => sub {
+		my ($self) = @_;
+		my $key = $self->environment_create();
+		die "Invalid session key created $key" unless ($key =~ /.+/);
+		return $key;
+	},
+);
+
+# consumerSecret - used to generate envionment and on created session
+has consumerSecret => (
 	is => 'rw',
 	required => 1,
 	default => 'guest',	# NOTE: Demo test environment (SIF-RS)
-};
+);
 
-has envornmentUser => {
+# consumerKey - Used only during creation of environment
+has consumerKey => (
 	is => 'rw',
 	required => 1,
 	default => 'new',	# NOTE: Demo test environment (SIF-RS)
-};
+);
+
+has solutionId => (
+	is => 'rw',
+	required => 1,
+	default => 'testSolution',
+);
+
+# environment_xml_send - generated or provided XML to generate a new environment
+has environment_xml_send => (
+	is => 'rw',
+	required => 1,
+	lazy => 1,
+	default => sub {
+		my ($self) = @_;
+		# TODO - what about setting better actual data?
+		return qq{<environment><solutionId>} . $self->solutionId . qq{</solutionId><authenticationMethod>Basic</authenticationMethod><consumerName>Perl</consumerName><applicationInfo><applicationKey>PERL</applicationKey><supportedInfrastructureVersion>3.0</supportedInfrastructureVersion><supportedDataModel>SIF-US</supportedDataModel><supportedDataModelVersion>3.0</supportedDataModelVersion><transport>REST</transport><applicationProduct><vendorName>X</vendorName><productName>X</productName><productVersion>X</productVersion></applicationProduct></applicationInfo></environment>};
+	},
+);
+
+# Kept in case we need to review results later
+has environment_xml_receive => (
+	is => 'rw',
+);
+has environment_data => (
+	is => 'rw',
+);
 
 # Create an environment
 # 	- Keeping return data?
 # 	- Storing sessionKey
 sub environment_create {
 	my ($self) = @_;
-	my $create_xml = q{<environment><solutionId>testSolution</solutionId><authenticationMethod>Basic</authenticationMethod><applicationInfo><applicationKey>MIKER_TEST</applicationKey><consumerName>MIKER_CONSUMERNAME</consumerName><supportedInfrastructureVersion>3.0</supportedInfrastructureVersion><supportedDataModel>SIF-US</supportedDataModel><supportedDataModelVersion>3.0</supportedDataModelVersion><transport>REST</transport><applicationProduct><vendorName>X</vendorName><productName>X</productName><productVersion>X</productVersion></applicationProduct></applicationInfo></environment>};
 
-	my $req = POST 
-		$self->endpoint . '/environments/environment', 
-		Content_Type => 'application/xml',
-		Accept => 'application/xml',
-		Content => $create_xml,
-	;
-	$req->authorization_basic($self->environmentUser, $self->sharedSecret);
-	my $response = $ua->request($req);
-	my $xml = $response->as_string; 
+	$self->rest->POST(
+		'environments/environment', 
+		$self->environment_xml_send,
+		{
+			'Authorization' => 'Basic '. encode_base64($self->consumerKey . ':' . $self->consumerSecret),
+		}
+	);
 
-	print STDERR $response->decoded_content;
-	my $ref = XMLin($response->decoded_content, ForceArray => 0);
-	use Data::Dumper;
-	print Dumper($ref);
-	my $key = $ref->{sessionToken} . "\n";
-	print STDERR "Done - $key\n";
-	return $key;
+	#say $client->responseContent();
+	$self->environment_xml_receive($self->rest->responseContent());
+	$self->environment_data($self->xml2data($self->environment_xml_receive));
+	return $self->environment_data->{sessionToken};
+}
+
+# NOTE: Simplified XML convert - see SIF::AU::XML instead !
+sub xml2data {
+	my ($self, $in) = @_;
+	return XMLin($in, ForceArray => 0);
+}
+
+sub setupRest {
+	my ($self) = @_;
+	$self->rest->addHeader('Authorization', 'Basic '.  encode_base64($self->sessionToken . ':' . $self->consumerSecret));
+
+	# XXX proper way here is to re-request environment data if not already known
+	# XXX e.g. if a user includes a sessionToken, then how do I get requestsConnector ? from Environments
+	# XXX But then I need an environment ID saved as well...
+	my $host = $self->environment_data->{infrastructureServices}{infrastructureService}{requestsConnector}{content};
+	die "Invalid hostname / endpoint = $host" unless ($host =~ /^https?:\/\/.+$/);
+	$self->rest->setHost($host);
 }
 
 # Basic Get - Collection,Id
 sub get {
 	my ($self, $object, $id) = @_;
 
-	my $req = GET 
-		$self->endpoint . '/' . $object . '/' . $id,	# TODO Safe to add '/' ?
-		Content_Type => 'application/xml',
-		Accept => 'application/xml',
-	;
-	$req->authorization_basic($self->sessionToken, $self->sharedSecret);
-	$response = $ua->request($req);
-	print $response->decoded_content;
-	my $students = XMLin($response->decoded_content, ForceArray => 0);
-	print join(", ", map { $_->{name}{nameOfRecord}{fullName} } @{$students->{student}} ) . "\n";
+	$self->rest->GET($object . ($id ? '/' . $id : ''));
+	return $self->rest->responseContent();
 }
 
 sub delete {
+	my ($self, $object, $id) = @_;
+	$self->rest->DELETE($object . '/' . $id);
+	return $self->rest->responseContent();
 }
 
 sub post {
+	my ($self, $object, $id, $data) = @_;
+	$self->rest->POST($object . ($id ? '/' . $id : ''), $data);
+	return $self->rest->responseContent();
 }
 
 sub put {
+	my ($self, $object, $id, $data) = @_;
+	$self->rest->POST($object . ($id ? '/' . $id : ''), $data);
+	return $self->rest->responseContent();
 }
 
 __PACKAGE__->meta->make_immutable();
---
+++ test_SIF_REST.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use perl5i::2;
+use lib 'lib';
+use SIF::REST;
+use XML::Simple;
+
+{
+	say "US ENDPOINT; US DATA STRUCTURE - Direct";
+	my $sifrest = SIF::REST->new({
+		endpoint => 'http://rest3api.sifassociation.org/api',
+	});
+	$sifrest->setupRest();
+
+	my $students = XMLin($sifrest->get('students'), ForceArray => 0);
+	print join(", ", map { $_->{name}{nameOfRecord}{fullName} } @{$students->{student}}[0..10] ) . "\n";
+}
+
+{
+	say "US ENDPOINT; AU DATA STRUCUTRE - Brokered";
+	my $sifrest = SIF::REST->new({
+		endpoint => 'http://rest3api.sifassociation.org/api',
+		solutionId => 'auTestSolution',
+	});
+
+	$sifrest->setupRest();
+
+	my $students = XMLin($sifrest->get('StudentPersonals'), ForceArray => 0);
+	print join(", ", map { $_->{PersonInfo}{Name}{FamilyName} } @{$students->{StudentPersonal}}[0..10] ) . "\n";
+}
+
+{
+	say "AU ENDPOINT; AU DATA STRUCUTRE - Direct";
+	my $sifrest = SIF::REST->new({
+		endpoint => 'http://siftraining.dd.com.au/api',
+		#endpoint => 'http://localhost:3000',
+		solutionId => 'auTestSolution',
+	});
+	$sifrest->setupRest();
+
+	my $students = XMLin($sifrest->get('StudentPersonals'), ForceArray => 0);
+	print join(", ", map { $_->{PersonInfo}{Name}{FamilyName} } @{$students->{StudentPersonal}}[0..10] ) . "\n";
+}
